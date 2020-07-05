0dfddf2784e1bbb1c39aa0960d17c3cc494cfadd
jjules2010 <mwamba1996@gmail.com>
2020-07-05 16:20:48 +0200

    Appel serveur, afin de récupérer la couleur des pokemon.

--- app/src/main/java/com/github/jjules2010/myapplication/MainActivity.java
+++ app/src/main/java/com/github/jjules2010/myapplication/MainActivity.java
@@ -6,11 +6,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://pokeapi.co";
 
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
@@ -21,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+     showList();
+     makeApiCall();
+    }
+    private void showList(){
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         // use this setting to
         // improve performance if you know that changes
@@ -41,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new ListAdapter(input);
         recyclerView.setAdapter(mAdapter);
 
+
         // put this after your definition of your recyclerview
 // input in your data mode in this example a java.util.List, adjust if necessary
 // adapter is your adapter
@@ -59,9 +76,46 @@ public class MainActivity extends AppCompatActivity {
                 };
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
         itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
 
 
+    private void makeApiCall(){
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        PokeApi pokeApi = retrofit.create(PokeApi.class);
+
+        Call<RestPokemonResponse> call = pokeApi.getPokemonResponse();
+        call.enqueue(new Callback<RestPokemonResponse>() {
+            @Override
+            public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    List<Pokemon> pokemonList=response.body().getResults();
+                    Toast.makeText(getApplicationContext(), "API Success", Toast.LENGTH_SHORT).show();
+
+
+                }else {
+                    showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+                showError();
+
+            }
+        });
+
+    }
+
+    private void showError() {
+        Toast.makeText(getApplicationContext(), "API error", Toast.LENGTH_SHORT).show();
 
     }
 }
---
+++ app/src/main/java/com/github/jjules2010/myapplication/Pokemon.java
@@ -0,0 +1,14 @@
+package com.github.jjules2010.myapplication;
+
+public class Pokemon {
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
